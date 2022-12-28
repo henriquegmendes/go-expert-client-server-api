ea7ffe9f9d66c560752094f50477050178b46136
Henrique Guazzelli Mendes <henrique@verifymyage.co.uk>
2022-12-28 15:29:09 -0300

    hide gorm debug logs

--- server/database/sqlite.go
+++ server/database/sqlite.go
@@ -4,12 +4,13 @@ import (
 	"github.com/henriquegmendes/go-expert-client-server-api/server/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
 )
 
 func ConnectToDatabase() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("sample.db"), &gorm.Config{
-		//Logger: logger.Default.LogMode(logger.Silent),
+		Logger: logger.Default.LogMode(logger.Silent),
 	})
 	if err != nil {
 		log.Fatalf("can't connect to database. error: %s", err.Error())
